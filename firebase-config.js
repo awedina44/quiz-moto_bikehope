ac38d1a08edadfb465027130fc7bb776b7456dc5
awedina44 <159126587+awedina44@users.noreply.github.com>
2025-07-20 19:04:22 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDQKrs5BTCg_KOkOg5Qhm1xDjk0O9-XzAA",
+  authDomain: "quiz-moto-9aedd.firebaseapp.com",
+  projectId: "quiz-moto-9aedd",
+  storageBucket: "quiz-moto-9aedd.firebasestorage.app",
+  messagingSenderId: "113444665192",
+  appId: "1:113444665192:web:81afbddbf9062cdb0a08dd",
+  measurementId: "G-VNH2D9FEDF"
+};
